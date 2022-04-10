c83800d54389138f9172f8c9189683b63eba1c72
AlexiSkyline <ilegal_sprite@outlook.com>
2022-04-10 16:12:48 -0500

    Add RestTemplete in Customer

--- customer/src/main/java/com/alexiskyline/customer/CustomerService.java
+++ customer/src/main/java/com/alexiskyline/customer/CustomerService.java
@@ -2,11 +2,13 @@ package com.alexiskyline.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer( CustomerRegistrationRequest request ) {
         Customer customer = Customer.builder()
@@ -14,7 +16,19 @@ public class CustomerService {
                 .lastName( request.lastName() )
                 .email( request.email() )
                 .build();
+        // Todo: check if email valid
+        // Todo: check if email not token
+        customerRepository.saveAndFlush( customer );
+        // Todo check if fraud
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
 
-        customerRepository.save( customer );
+        if( fraudCheckResponse.isFraudster() ) {
+            throw  new IllegalStateException( "fraudster" );
+        }
+        // Todo: send notification
     }
 }
---
+++ customer/src/main/java/com/alexiskyline/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.alexiskyline.customer;
+
+public record FraudCheckResponse(Boolean isFraudster ) {
+}
